946e3d713f0a8b1b200a017e9d84f7acab8699ec
Adrian Edwards <aedwards92@gmail.com>
2011-11-16 22:35:20 -0500

    fixed typo on the status field

--- public_html/applications/init/models/db_fc.py
+++ public_html/applications/init/models/db_fc.py
@@ -8,7 +8,7 @@ db.define_table('events',
     Field('end_date','date'),
     Field('participants', 'list:reference auth_user'),
     Field('check_in', 'string'),
-    Field('status', 'string', default='Pending'),
+    Field('status', 'string'),
     Field('metric_type', 'string'),
     auth.signature,
     format='%(name)s')
@@ -17,7 +17,7 @@ db.define_table('events',
 db.events.participants._format = lambda row: "%s %s" % (row.first_name, row.last_name)
 #db.events.participants._format = '%(first_name)s %(last_name)s'
 db.events.check_in.requires=IS_IN_SET(('Daily','Weekly','Monthly'))
-db.events.status.requires=IS_IN_SET(('Pending','In-Progress','Ended'))
+db.events.status.requires=IS_EMPTY_OR(IS_IN_SET(('Pending','In-Progress','Ended')))
 db.events.metric_type.requires=IS_IN_SET(('Reps/Laps','Time','BMI', '% Weight Loss', 'lbs lost'))
 
 db.define_table('event_status',
